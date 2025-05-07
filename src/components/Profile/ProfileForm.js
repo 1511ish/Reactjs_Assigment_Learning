ede4c0643c3cb80852d4d2f7accf72322427fdaa
Ishant Pal <mrishantsre1511ish@gmail.com>
2025-05-07 23:38:50 +0530

    added reset password feature

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,14 +1,26 @@
+import { useRef, useState } from 'react';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const passRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredPassword = passRef.current.value;
+    setIsLoading(true);
+    setTimeout(() => {
+      alert(`New Password: ${enteredPassword}`);
+      setIsLoading(false);
+    }, 500)
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={passRef} required />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        {!isLoading ? <button>Change Password</button> : <p>Sending Request...</p>}
       </div>
     </form>
   );
