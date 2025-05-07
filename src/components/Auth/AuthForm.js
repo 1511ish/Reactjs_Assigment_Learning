48c223cc0456969c3f951bfb549c3165e5a931c4
Ishant Pal <mrishantsre1511ish@gmail.com>
2025-05-07 23:18:37 +0530

    How to Authenticate

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,70 @@
+import { useState, useRef } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const emailInputRef = useRef();
+  const passInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passInputRef.current.value;
+
+    setIsLoading(true);
+    if (isLogin) {
+      setTimeout(() => {
+        alert(`${enteredEmail} is loggedIn`)
+        setIsLoading(false);
+      }, 500);
+    } else {
+      setTimeout(() => {
+        alert(`${enteredEmail} is Signuped.`)
+        setIsLoading(false);
+      }, 500);
+    }
+
+  }
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' ref={emailInputRef} required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            ref={passInputRef}
+            required
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && <button>{isLogin ? 'LogIn' : 'Create Account'}</button>}
+          {isLoading && <p>Sending Request...</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
